fb8a6d43ff01b1574a2e27b3604bad039ba1045f
khawlah alrashed <khawlah_alrashed@hotmail.com>
2022-04-18 06:44:35 +0300

    Initial Commit

---
+++ MetaWeatherApp/MetaWeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MetaWeatherAppApp.swift
+//  MetaWeatherApp
+//
+//  Created by Khawlah Alrashed on 18/04/2022.
+//
+
+import SwiftUI
+
+@main
+struct MetaWeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
